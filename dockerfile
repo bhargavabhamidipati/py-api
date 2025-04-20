3b14a5332a388e2b5a1e6c89b3cce46499f1ecc0
bhargavabhamidipati <bhargavabhamidipati@gmail.com>
2025-04-20 21:11:48 +0200

    changed the port numbers to 8080 in cloud run.

--- dockerfile
+++ dockerfile
@@ -16,7 +16,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 
 RUN pip install -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 8080
 
 CMD ["python", "main.py"]
 
--- main.py
+++ main.py
@@ -22,5 +22,5 @@ app.register_blueprint(palindrome, url_prefix=URL_PREFIX)
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=8080)
     #app.run(debug=True, host='0.0.0.0', port=5000)
