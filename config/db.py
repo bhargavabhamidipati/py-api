f69e415407ecdb2ad6bbbcf827da55e962a3cedd
bhargavabhamidipati <bhargavabhamidipati@gmail.com>
2025-04-22 18:22:03 +0200

    cleaned workflow and added decoding to db.py

--- config/db.py
+++ config/db.py
@@ -3,8 +3,10 @@ from contextlib import contextmanager
 from flask import current_app
 import json
 import os
+import base64
 
-firebase_config_str = os.getenv("FIREBASE_CONFIG")
+firebase_config_b64 = os.getenv("FIREBASE_CONFIG_B64")
+firebase_config_str = base64.b64decode(firebase_config_b64).decode()
 firebase_config = json.loads(firebase_config_str)
 
 @contextmanager
